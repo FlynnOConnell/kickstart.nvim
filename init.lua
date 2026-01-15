fc84b7bf5270ae2d483a56016b29cab26436513b
Flynn OConnell <flynnoconnell@gmail.com>
2026-01-15 14:19:36 -0500

    Remove ruff from Mason (use uv-installed ruff)

--- init.lua
+++ init.lua
@@ -676,7 +676,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'ruff',   -- Python linter + formatter (replaces flake8, black, isort)
+        -- ruff installed via 'uv tool install ruff' (not Mason)
         'markdownlint',
         'matlab-language-server',
         'prettier',
