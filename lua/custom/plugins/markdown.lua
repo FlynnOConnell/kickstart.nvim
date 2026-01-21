0dd68ec5cd41dc628b802129f15dd414a04d9dcf
Flynn OConnell <flynnoconnell@gmail.com>
2026-01-21 12:09:27 -0500

    auto-commit from dotfiles push

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,16 +1,15 @@
 -- Markdown utilities: image paste and code block wrappers
 local vault_path = vim.fn.expand '~/repos/docs'
 
-local M = {}
-
-function M.wrap_python()
+-- Make wrap_python globally accessible
+_G.wrap_python_codeblock = function()
   local start_line = vim.fn.line "'<"
   local end_line = vim.fn.line "'>"
   vim.api.nvim_buf_set_lines(0, end_line, end_line, false, { '```' })
   vim.api.nvim_buf_set_lines(0, start_line - 1, start_line - 1, false, { '```python' })
 end
 
-M.spec = {
+return {
   {
     'HakonHarnes/img-clip.nvim',
     event = 'VeryLazy',
@@ -29,9 +28,18 @@ M.spec = {
     },
     keys = {
       { '<leader>P', '<cmd>PasteImage<cr>', desc = '[P]aste image from clipboard' },
-      { '<leader>C', "<esc><cmd>lua require('custom.plugins.markdown').wrap_python()<cr>", desc = 'Wrap in python [C]ode block', mode = 'v' },
+      { '<leader>C', '<esc><cmd>lua wrap_python_codeblock()<cr>', desc = 'Wrap in python [C]ode block', mode = 'v' },
+    },
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+    keys = {
+      { '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', desc = '[M]arkdown [P]review toggle' },
     },
   },
 }
-
-return M.spec
